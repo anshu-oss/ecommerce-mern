5785cf95bd4878d152d8aff9aecdf557aef4e00a
anash khan <anashkhankhan037@gmail.com>
2026-06-22 20:36:22 +0530

    HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -24,33 +24,12 @@ export default function HomeScreen() {
 
   useEffect(() => {
     dispatch(listProducts({}));
-    dispatch(listTopSellers());
+   // dispatch(listTopSellers());
   }, [dispatch]);
   return (
     <div>
-      <h2>Top Sellers</h2>
-      {loadingSellers ? (
-        <LoadingBox></LoadingBox>
-      ) : errorSellers ? (
-        <MessageBox variant="danger">{errorSellers}</MessageBox>
-      ) : (
-        <>
-         {(!Array.isArray(sellers) || sellers.length === 0) && (
-  <MessageBox>No Seller Found</MessageBox>
-)}
-         <Carousel showArrows autoPlay showThumbs={false}>
-  {Array.isArray(sellers) &&
-    sellers.map((seller) => (
-              <div key={seller._id}>
-                <Link to={`/seller/${seller._id}`}>
-                  <img src={seller.seller.logo} alt={seller.seller.name} />
-                  <p className="legend">{seller.seller.name}</p>
-                </Link>
-              </div>
-            ))}
-          </Carousel>
-        </>
-      )}
+     
+        
       <h2>Featured Products</h2>
       {loading ? (
         <LoadingBox></LoadingBox>
